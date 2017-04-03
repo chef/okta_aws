849f1567dbfaadce1cfadf3f62cc103342ad405a
Mark Harrison <mark@mivok.net>
2017-04-03 15:33:15 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='okta_aws',
+    version='0.1.0',
+    description='Use the AWS API via an account using Okta',
+    author='Mark Harrison',
+    scripts=['okta_aws'],
+    url='https://github.com/chef/okta_aws',
+    install_requires=['oktaauth>=0.2']
+)
