f7a04e1e5d8f7f69b93340337ef77d6982324d9c
Mark Harrison <mark@mivok.net>
2018-02-08 17:37:01 -0500

    Fix dependencies

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     author='Mark Harrison',
     scripts=['okta_aws'],
     url='https://github.com/chef/okta_aws',
-    install_requires=['oktaauth>=0.2', 'toml>=0.9.2']
+    install_requires=['requests>=2.18.4', 'toml>=0.9.4']
 )
