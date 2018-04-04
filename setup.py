1e8fcb34a9507d33bc3398142d19c9c924f8a935
Mark Harrison <mark@mivok.net>
2018-04-04 14:35:10 -0400

    Setup.py updates

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     version='0.2.0',
     description='Use the AWS API via an account using Okta',
     author='Mark Harrison',
+    author_email='mharrison@chef.io',
+    license='Apache-2.0',
     scripts=['okta_aws'],
     url='https://github.com/chef/okta_aws',
+    python_requires='>=3',
     install_requires=['requests>=2.18.4', 'toml>=0.9.4']
 )
